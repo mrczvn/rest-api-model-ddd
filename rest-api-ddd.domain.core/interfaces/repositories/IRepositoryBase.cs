3e5e6a9ec18ac6f0a18173edb6b8fecd3d2bcb99
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-20 18:55:56 -0300

    chore: added summary to the file

--- rest-api-ddd.domain.core/interfaces/repositories/IRepositoryBase.cs
+++ rest-api-ddd.domain.core/interfaces/repositories/IRepositoryBase.cs
@@ -5,28 +5,31 @@ namespace rest_api_ddd.domain.core.interfaces.repositories
     public interface IRepositoryBase<TEntity> where TEntity : class
     {
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="obj"></param>
         void Add(TEntity obj);
+
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="obj"></param>
         void Update(TEntity obj);
+
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="obj"></param>
         void Remove(TEntity obj);
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <returns></returns>
         IEnumerable<TEntity> GetAll();
+
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
