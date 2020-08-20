0fee9e74febbf09f2d329b7a1a14009cf459c939
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-20 19:06:25 -0300

    feat: client repository interface created

---
+++ rest-api-ddd.domain.core/interfaces/repositories/IRepositoryClient.cs
@@ -0,0 +1,8 @@
+﻿using rest_api_ddd.domain.entitys;
+
+namespace rest_api_ddd.domain.core.interfaces.repositories
+{
+    public interface IRepositoryClient : IRepositoryBase<Client>
+    {
+    }
+}
