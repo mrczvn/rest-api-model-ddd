2054394cebc9a13790286b6069776361a16b25f5
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-20 19:06:12 -0300

    feat: product repository interface created

---
+++ rest-api-ddd.domain.core/interfaces/repositories/IRepositoryProduct.cs
@@ -0,0 +1,11 @@
+﻿using rest_api_ddd.domain.entitys;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rest_api_ddd.domain.core.interfaces.repositories
+{
+    public interface IRepositoryProduct : IRepositoryBase<Product>
+    {
+    }
+}
