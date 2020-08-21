66716b7d6cc751193fdc3aab9e3136b2ad7b7851
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-21 10:26:40 -0300

    feat: product mapper interface created

---
+++ rest-api-ddd.infrastructure/cross-cutting/interfaces/IMapperProduct.cs
@@ -0,0 +1,30 @@
+﻿using rest_api_ddd.application.dtos;
+using rest_api_ddd.domain.entitys;
+using System.Collections.Generic;
+
+namespace rest_api_ddd.infrastructure.cross_cutting.interfaces
+{
+    public interface IMapperProduct
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="productDto"></param>
+        /// <returns></returns>
+        Product MapperDtoEntity(ProductDto productDto);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="products"></param>
+        /// <returns></returns>
+        IEnumerable<ProductDto> MapperListProductsDto(IEnumerable<Product> products);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        Product MapperEntityToDto(Product product);
+    }
+}
