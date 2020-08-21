48880f129e2c88853d7da4ba20d54d71ddb2e4b2
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-21 10:23:14 -0300

    feat: client mapper interface created

---
+++ rest-api-ddd.infrastructure/cross-cutting/interfaces/IMapperClient.cs
@@ -0,0 +1,30 @@
+﻿using rest_api_ddd.domain.entitys;
+using rest_api_ddd.domain.services;
+using System.Collections.Generic;
+
+namespace rest_api_ddd.infrastructure.cross_cutting.interfaces
+{
+    public interface IMapperClient
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="clientDto"></param>
+        /// <returns></returns>
+        Client MapperDtoEntity(ClientDto clientDto);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="clients"></param>
+        /// <returns></returns>
+        IEnumerable<ClientDto> MapperListClientsDto(IEnumerable<Client> clients);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        ClientDto MapperEntityToDto(Client client);
+    }
+}
