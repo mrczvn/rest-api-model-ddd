339d6e1ed600a2f29a32c36359c42bf1e6147281
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-20 23:15:54 -0300

    feat: generic services interface created

---
+++ rest-api-ddd.domain.core/interfaces/services/IServiceBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace rest_api_ddd.domain.core.interfaces.services
+{
+    public interface IServiceBase<TEntity> where TEntity : class
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="obj"></param>
+        void Add(TEntity obj);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="obj"></param>
+        void Update(TEntity obj);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="obj"></param>
+        void Remove(TEntity obj);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<TEntity> GetAll();
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        TEntity GetById(int id);
+    }
+}
