382da6ccc2dbd0799c1317cc72f02f148cb42693
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-21 11:49:25 -0300

    feat: creating product from my data transfer object

---
+++ rest-api-ddd.infrastructure/cross-cutting/mapper/MapperProduct.cs
@@ -0,0 +1,40 @@
+﻿using rest_api_ddd.application.dtos;
+using rest_api_ddd.domain.entitys;
+using rest_api_ddd.infrastructure.cross_cutting.interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace rest_api_ddd.infrastructure.cross_cutting.mapper
+{
+    public class MapperProduct : IMapperProduct
+    {
+        private IEnumerable<ProductDto> productDtos = new List<ProductDto>();
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="productDto"></param>
+        /// <returns></returns>
+        public Product MapperDtoEntity(ProductDto productDto)
+        {
+            var product = new Product()
+            {
+                Id = productDto.Id,
+                Name = productDto.Name,
+                Value = productDto.Value
+            };
+
+            return product;
+        }
+
+        public Product MapperEntityToDto(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ProductDto> MapperListProductsDto(IEnumerable<Product> products)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
