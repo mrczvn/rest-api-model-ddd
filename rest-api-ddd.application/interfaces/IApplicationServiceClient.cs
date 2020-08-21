fde4e0e588f4c2b7cfc6e87210e5820f91adbe77
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-21 10:09:43 -0300

    feat: application service interface created

---
+++ rest-api-ddd.application/interfaces/IApplicationServiceClient.cs
@@ -0,0 +1,38 @@
+﻿using rest_api_ddd.domain.services;
+using System.Collections.Generic;
+
+namespace rest_api_ddd.application.interfaces
+{
+    public interface IApplicationServiceClient
+    {   /// <summary>
+        ///
+        /// </summary>
+        /// <param name="clientDto"></param>
+        void Add(ClientDto clientDto);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="clientDto"></param>
+        void Update(ClientDto clientDto);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="clientDto"></param>
+        void Remove(ClientDto clientDto);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<ClientDto> GetAll();
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        ClientDto GetById(int id);
+    }
+}
