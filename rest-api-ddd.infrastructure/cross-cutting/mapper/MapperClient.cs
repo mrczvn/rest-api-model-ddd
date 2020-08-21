fa21049a235ef359e6aa36fa4a935572299843f3
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-21 10:33:42 -0300

    feat: created method that created a new client

---
+++ rest-api-ddd.infrastructure/cross-cutting/mapper/MapperClient.cs
@@ -0,0 +1,40 @@
+﻿using rest_api_ddd.domain.entitys;
+using rest_api_ddd.domain.services;
+using rest_api_ddd.infrastructure.cross_cutting.interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace rest_api_ddd.infrastructure.cross_cutting.mapper
+{
+    public class MapperClient : IMapperClient
+    {
+        private IEnumerable<ClientDto> clientDtos = new List<ClientDto>();
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="clientDto"></param>
+        /// <returns></returns>
+        public Client MapperDtoEntity(ClientDto clientDto)
+        {
+            var client = new Client()
+            {
+                Id = clientDto.Id,
+                Name = clientDto.Name,
+                Surname = clientDto.Surname,
+                Email = clientDto.Email
+            };
+            return client;
+        }
+
+        public ClientDto MapperEntityToDto(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ClientDto> MapperListClientsDto(IEnumerable<Client> clients)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
