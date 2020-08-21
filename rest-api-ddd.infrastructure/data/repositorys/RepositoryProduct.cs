fd694ecc157f72dc3556055f3c612b93da1e050e
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-20 23:11:04 -0300

    feat: created product class inheriting from the base repository

---
+++ rest-api-ddd.infrastructure/data/repositorys/RepositoryProduct.cs
@@ -0,0 +1,20 @@
+﻿using rest_api_ddd.domain.core.interfaces.repositories;
+using rest_api_ddd.domain.entitys;
+
+namespace rest_api_ddd.infrastructure.data.repositorys
+{
+    public class RepositoryProduct : RepositoryBase<Product>, IRepositoryProduct
+    {
+        private readonly SqlContext sqlContext;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="sqlContext"></param>
+        public RepositoryProduct(SqlContext sqlContext)
+            : base(sqlContext)
+        {
+            this.sqlContext = sqlContext;
+        }
+    }
+}
