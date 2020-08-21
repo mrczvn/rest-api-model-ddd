51c8d5bd49aa9551fab47c1e8e69ed2724323e02
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-20 23:27:02 -0300

    feat: created product interface that inherits from the generic interface

---
+++ rest-api-ddd.domain.core/interfaces/services/IServiceProduct.cs
@@ -0,0 +1,11 @@
+﻿using rest_api_ddd.domain.entitys;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rest_api_ddd.domain.core.interfaces.services
+{
+    public interface IServiceProduct : IServiceBase<Product>
+    {
+    }
+}
---
+++ rest-api-ddd.domain.services/ServiceBase.cs
@@ -0,0 +1,50 @@
+﻿using rest_api_ddd.domain.core.interfaces.repositories;
+using rest_api_ddd.domain.core.interfaces.services;
+using System;
+using System.Collections.Generic;
+
+namespace rest_api_ddd.domain.services
+{
+    public class ServiceBase<TEntity> : IServiceBase<TEntity> where TEntity : class
+    {
+        private readonly IRepositoryBase<TEntity> repository;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="repository"></param>
+        public ServiceBase(IRepositoryBase<TEntity> repository)
+        {
+            this.repository = repository;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="obj"></param>
+        public void Add(TEntity obj)
+        {
+            repository.Add(obj);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
