ddec8b8c1686cb533e8bf995ed84ccc33e71f60b
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-21 10:03:25 -0300

    feat: created product DTO

---
+++ rest-api-ddd.application/dtos/ProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace rest_api_ddd.application.dtos
+{
+    public class ProductDto
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+    }
+}
