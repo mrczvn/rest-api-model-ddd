57195e17d62f8e4fa9e410abd72b59ef0674a312
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-21 10:14:28 -0300

    feat: application service product interface created

---
+++ rest-api-ddd.application/interfaces/IApplicationServiceProduct.cs
@@ -0,0 +1,38 @@
+﻿using rest_api_ddd.application.dtos;
+using System.Collections.Generic;
+
+namespace rest_api_ddd.application.interfaces
+{
+    public interface IApplicationServiceProduct
+    {   /// <summary>
+        ///
+        /// </summary>
+        /// <param name="productDto"></param>
+        void Add(ProductDto productDto);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="productDto"></param>
+        void Update(ProductDto productDto);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="productDto"></param>
+        void Remove(ProductDto productDto);
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<ProductDto> GetAll();
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        ProductDto GetById(int id);
+    }
+}
