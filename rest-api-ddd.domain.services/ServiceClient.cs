e14607fe8208e1fb0f9cde38f3f84d1dbdf7225a
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-21 09:52:53 -0300

    feat: creating customer service class inheriting from base service class

---
+++ rest-api-ddd.domain.services/ServiceClient.cs
@@ -0,0 +1,16 @@
+﻿using rest_api_ddd.domain.core.interfaces.repositories;
+using rest_api_ddd.domain.entitys;
+
+namespace rest_api_ddd.domain.services
+{
+    public class ServiceClient : ServiceBase<Client>, IRepositoryClient
+    {
+        private readonly IRepositoryClient repositoryClient;
+
+        public ServiceClient(IRepositoryClient repositoryClient)
+            : base(repositoryClient)
+        {
+            this.repositoryClient = repositoryClient;
+        }
+    }
+}
