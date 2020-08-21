edde8f4d1189432675784590c79d1ce7194d1f16
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-21 09:55:29 -0300

    feat: creating product service class inheriting from base service class

---
+++ rest-api-ddd.domain.services/ServiceProduct.cs
@@ -0,0 +1,17 @@
+﻿using rest_api_ddd.domain.core.interfaces.repositories;
+using rest_api_ddd.domain.core.interfaces.services;
+using rest_api_ddd.domain.entitys;
+
+namespace rest_api_ddd.domain.services
+{
+    public class ServiceProduct : ServiceBase<Product>, IServiceProduct
+    {
+        private readonly IRepositoryProduct repositoryProduct;
+
+        public ServiceProduct(IRepositoryProduct repositoryProduct)
+            : base(repositoryProduct)
+        {
+            this.repositoryProduct = repositoryProduct;
+        }
+    }
+}
