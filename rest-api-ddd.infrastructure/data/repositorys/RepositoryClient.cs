4eb5931c67633c02f0faaaf4b53fb2cf207d88a7
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-20 23:10:45 -0300

    feat: created client class inheriting from the base repository

---
+++ rest-api-ddd.infrastructure/data/repositorys/RepositoryClient.cs
@@ -0,0 +1,20 @@
+﻿using rest_api_ddd.domain.core.interfaces.repositories;
+using rest_api_ddd.domain.entitys;
+
+namespace rest_api_ddd.infrastructure.data.repositorys
+{
+    public class RepositoryClient : RepositoryBase<Client>, IRepositoryClient
+    {
+        private readonly SqlContext sqlContext;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="sqlContext"></param>
+        public RepositoryClient(SqlContext sqlContext)
+            : base(sqlContext)
+        {
+            this.sqlContext = sqlContext;
+        }
+    }
+}
