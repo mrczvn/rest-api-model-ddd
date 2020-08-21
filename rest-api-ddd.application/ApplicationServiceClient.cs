75c1c42b0a731f503dffc5ae3e454ed8a4ac9ff9
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-21 13:24:00 -0300

    chore: summary applied to class ApplicationServiceClient

--- rest-api-ddd.application/ApplicationServiceClient.cs
+++ rest-api-ddd.application/ApplicationServiceClient.cs
@@ -2,9 +2,7 @@
 using rest_api_ddd.application.interfaces;
 using rest_api_ddd.application.interfaces.mappers;
 using rest_api_ddd.domain.core.interfaces.services;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace rest_api_ddd.application
 {
@@ -13,35 +11,62 @@ namespace rest_api_ddd.application
         private readonly IServiceClient serviceClient;
         private readonly IMapperClient mapperClient;
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="serviceClient"></param>
+        /// <param name="mapperClient"></param>
         public ApplicationServiceClient(IServiceClient serviceClient, IMapperClient mapperClient)
         {
             this.serviceClient = serviceClient;
             this.mapperClient = mapperClient;
         }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="clientDto"></param>
         public void Add(ClientDto clientDto)
         {
             var client = mapperClient.MapperDtoToEntity(clientDto);
             serviceClient.Add(client);
         }
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<ClientDto> GetAll()
         {
             var clients = serviceClient.GetAll();
             return mapperClient.MapperListClientsDto(clients);
         }
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public ClientDto GetById(int id)
         {
             var client = serviceClient.GetById(id);
             return mapperClient.MapperEntityToDto(client);
         }
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="clientDto"></param>
         public void Remove(ClientDto clientDto)
         {
             var client = mapperClient.MapperDtoToEntity(clientDto);
             serviceClient.Remove(client);
         }
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="clientDto"></param>
         public void Update(ClientDto clientDto)
         {
             var client = mapperClient.MapperDtoToEntity(clientDto);
