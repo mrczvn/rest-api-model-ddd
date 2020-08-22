5610a5aeaf4fa55d55bc75d019dd52f4c10bf74a
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-21 21:44:48 -0300

    ensure that the GetAll method returns a list of products

--- rest-api-ddd.application/ApplicationServiceProduct.cs
+++ rest-api-ddd.application/ApplicationServiceProduct.cs
@@ -4,30 +4,38 @@ using rest_api_ddd.application.interfaces.mappers;
 using rest_api_ddd.domain.core.interfaces.services;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace rest_api_ddd.application
 {
-    class ApplicationServiceProduct : IApplicationServiceProduct
+    internal class ApplicationServiceProduct : IApplicationServiceProduct
     {
         private readonly IServiceProduct serviceProduct;
         private readonly IMapperProduct mapperProduct;
-
 
         public ApplicationServiceProduct(IServiceProduct serviceProduct, IMapperProduct mapperProduct)
         {
             this.serviceProduct = serviceProduct;
             this.mapperProduct = mapperProduct;
         }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="productDto"></param>
         public void Add(ProductDto productDto)
         {
             var product = mapperProduct.MapperDtoToEntity(productDto);
             serviceProduct.Add(product);
         }
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<ProductDto> GetAll()
         {
-            throw new NotImplementedException();
+            var products = serviceProduct.GetAll();
+            return mapperProduct.MapperListProductsDto(products)
         }
 
         public ProductDto GetById(int id)
