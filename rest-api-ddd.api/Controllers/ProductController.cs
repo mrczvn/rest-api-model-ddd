d876f8df245246e3bac23e088bbf3cf3be77d84e
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-24 13:46:03 -0300

    feat: ensure that the HttpGet method on the product route returns a list of products

---
+++ rest-api-ddd.api/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using rest_api_ddd.application.interfaces;
+
+namespace rest_api_ddd.api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly IApplicationServiceProduct _applicationServiceProduct;
+        public ProductController(IApplicationServiceProduct ApplicationServiceProduct)
+        {
+            _applicationServiceProduct = ApplicationServiceProduct;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_applicationServiceProduct.GetAll());
+        }
+    }
+}
