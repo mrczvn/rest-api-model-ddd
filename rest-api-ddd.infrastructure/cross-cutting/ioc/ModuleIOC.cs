c3484f304383745aed338294813301c4d8a0c4fb
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-24 12:57:19 -0300

    chore: new version of AutoFac and Microsoft.Entity installed

---
+++ rest-api-ddd.infrastructure/cross-cutting/ioc/ModuleIOC.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace rest_api_ddd.infrastructure.cross_cutting.ioc
+{
+    public class ModuleIOC : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            ConfigurationIOC.Load(builder);
+        }
+    }
+}
