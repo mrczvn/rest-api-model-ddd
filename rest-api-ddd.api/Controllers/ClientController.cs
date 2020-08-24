9a8a3165cd830a6d7f5c1f933be8b61b5a22db5f
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-24 12:45:13 -0300

    feat: ensure that the HttpGet method on the client route returns a list of clients

---
+++ rest-api-ddd.api/Controllers/ClientController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using rest_api_ddd.application.interfaces;
+using System.Collections.Generic;
+
+namespace rest_api_ddd.api.Controllers
+{
+    [Route("client")]
+    [ApiController]
+    public class ClientController : Controller
+    {
+        private readonly IApplicationServiceClient _applicationServiceClient;
+
+        public ClientController(IApplicationServiceClient ApplicationServiceClient)
+        {
+            _applicationServiceClient = ApplicationServiceClient;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_applicationServiceClient.GetAll());
+        }
+    }
+}
