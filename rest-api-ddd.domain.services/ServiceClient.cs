c4bbfbe6b6f80477078ffebad8499a914f4799c9
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-24 12:24:22 -0300

    chore: assigned reference to IServiceClient interface

--- rest-api-ddd.domain.services/ServiceClient.cs
+++ rest-api-ddd.domain.services/ServiceClient.cs
@@ -1,9 +1,10 @@
 ﻿using rest_api_ddd.domain.core.interfaces.repositories;
+using rest_api_ddd.domain.core.interfaces.services;
 using rest_api_ddd.domain.entitys;
 
 namespace rest_api_ddd.domain.services
 {
-    public class ServiceClient : ServiceBase<Client>, IRepositoryClient
+    public class ServiceClient : ServiceBase<Client>, IServiceClient
     {
         private readonly IRepositoryClient repositoryClient;
 
