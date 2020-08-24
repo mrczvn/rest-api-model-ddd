5d3e0bbcbdfdf9ee6b3918a96906423fce712100
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-24 13:50:12 -0300

    feat: ensure that the HttpPost method adds a new product

--- rest-api-ddd.api/Controllers/ProductController.cs
+++ rest-api-ddd.api/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using rest_api_ddd.application.dtos;
 using rest_api_ddd.application.interfaces;
+using System;
 using System.Collections.Generic;
 
 namespace rest_api_ddd.api.Controllers
@@ -25,6 +27,23 @@ namespace rest_api_ddd.api.Controllers
         public ActionResult<string> Get(int id)
         {
             return Ok(_applicationServiceProduct.GetById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                if (productDto == null)
+                    return NotFound();
+
+                _applicationServiceProduct.Add(productDto);
+                return Ok("Produto Cadastrado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
